aa09e81460c39f52b6d17cf45f7dfbdd550f22e8
Charles Creac <naruske282@gmail.com>
2025-12-11 20:31:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6127056094-56264",
+  "appId": "1:957532731986:web:16a0221ffe74436d283d8b",
+  "apiKey": "AIzaSyDxnJnbusiwblgRLta_VL3ukvVrzFdDUuU",
+  "authDomain": "studio-6127056094-56264.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "957532731986"
+};
