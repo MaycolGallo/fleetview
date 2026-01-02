8fb480e765c0903d437949cffb66da26e5bef9c1
Charles Creac <naruske282@gmail.com>
2026-01-02 21:37:42 +0000

    Try fixing this error: `Runtime Error: Element type is invalid: expected

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,15 +1,15 @@
 
 "use client"
 
-import { ResizableHandle as ResizableHandlePrimitive, ResizablePanel as ResizablePanelPrimitive, ResizablePanelGroup as ResizablePanelGroupPrimitive } from "react-resizable-panels"
+import * as ResizablePrimitive from "react-resizable-panels"
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
 const ResizablePanelGroup = React.forwardRef<
-  React.ElementRef<typeof ResizablePanelGroupPrimitive>,
-  React.ComponentProps<typeof ResizablePanelGroupPrimitive>
+  React.ElementRef<typeof ResizablePrimitive.PanelGroup>,
+  React.ComponentProps<typeof ResizablePrimitive.PanelGroup>
 >(({ className, ...props }, ref) => (
-  <ResizablePanelGroupPrimitive
+  <ResizablePrimitive.PanelGroup
     ref={ref}
     className={cn(
       "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
@@ -20,13 +20,13 @@ const ResizablePanelGroup = React.forwardRef<
 ))
 ResizablePanelGroup.displayName = "ResizablePanelGroup"
 
-const ResizablePanel = ResizablePanelPrimitive
+const ResizablePanel = ResizablePrimitive.Panel
 
 const ResizableHandle = React.forwardRef<
-  React.ElementRef<typeof ResizableHandlePrimitive>,
-  React.ComponentProps<typeof ResizableHandlePrimitive> & { withHandle?: boolean }
+  React.ElementRef<typeof ResizablePrimitive.PanelResizeHandle>,
+  React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & { withHandle?: boolean }
 >(({ className, withHandle, ...props }, ref) => (
-  <ResizableHandlePrimitive
+  <ResizablePrimitive.PanelResizeHandle
     ref={ref}
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-id]]:focus-visible:ring-offset-0",
@@ -39,11 +39,8 @@ const ResizableHandle = React.forwardRef<
         <div className="h-2.5 w-px bg-muted-foreground" />
       </div>
     )}
-  </ResizableHandlePrimitive>
+  </ResizablePrimitive.PanelResizeHandle>
 ))
 ResizableHandle.displayName = "ResizableHandle"
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
-
-
-    
