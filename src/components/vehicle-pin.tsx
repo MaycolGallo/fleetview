918e8f39ca70fffa73fdeaf5d7ffaf488a3a775a
Charles Creac <naruske282@gmail.com>
2026-01-10 01:56:21 +0000

    dont use this nigger inset-0

--- src/components/vehicle-pin.tsx
+++ src/components/vehicle-pin.tsx
@@ -27,10 +27,10 @@ export const VehiclePin = React.memo(({ vehicle, isSelected }: VehiclePinProps) 
         transition={{ type: "spring", stiffness: 400, damping: 17 }}
       >
           {/* Background Pin Shape */}
-          <div className="absolute inset-0">
+          <div className="absolute top-0 left-0 w-10 h-12">
              <svg
                 viewBox="0 0 38 54"
-                className="w-10 h-12 drop-shadow-lg"
+                className="w-full h-full drop-shadow-lg"
               >
               <path 
                   fill={color}
@@ -43,9 +43,10 @@ export const VehiclePin = React.memo(({ vehicle, isSelected }: VehiclePinProps) 
           
           {/* Centered Icon with counter-rotation */}
           <motion.div 
-            className="absolute inset-0 flex items-center justify-center pb-[14px]"
+            className="absolute top-0 left-0 w-10 h-10 flex items-center justify-center"
             animate={{ rotate: -rotation }}
             transition={{ type: "spring", stiffness: 400, damping: 17 }}
+            style={{paddingBottom: '11px'}}
           >
               <div className='w-7 h-7 bg-white rounded-full flex items-center justify-center'>
                 <Car className="w-5 h-5" style={{ color: color }} />
