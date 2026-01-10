a9e394974acf552338a1ae494597e1ccef1f636b
Charles Creac <naruske282@gmail.com>
2026-01-10 01:57:09 +0000

    dont add fucking padding¨¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡

--- src/components/vehicle-pin.tsx
+++ src/components/vehicle-pin.tsx
@@ -42,16 +42,18 @@ export const VehiclePin = React.memo(({ vehicle, isSelected }: VehiclePinProps) 
           </div>
           
           {/* Centered Icon with counter-rotation */}
-          <motion.div 
-            className="absolute top-0 left-0 w-10 h-10 flex items-center justify-center"
-            animate={{ rotate: -rotation }}
-            transition={{ type: "spring", stiffness: 400, damping: 17 }}
-            style={{paddingBottom: '11px'}}
+          <div
+            className="absolute left-0 w-10 flex justify-center"
+            style={{ top: '7px' }}
           >
-              <div className='w-7 h-7 bg-white rounded-full flex items-center justify-center'>
-                <Car className="w-5 h-5" style={{ color: color }} />
-              </div>
-          </motion.div>
+            <motion.div 
+              className="w-7 h-7 bg-white rounded-full flex items-center justify-center"
+              animate={{ rotate: -rotation }}
+              transition={{ type: "spring", stiffness: 400, damping: 17 }}
+            >
+                  <Car className="w-5 h-5" style={{ color: color }} />
+            </motion.div>
+          </div>
       </motion.div>
   );
 });
