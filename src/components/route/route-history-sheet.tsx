2a7c436d97e95b55271e82e63a7bf103a3f12f86
Charles Creac <naruske282@gmail.com>
2026-01-27 17:18:46 +0000

    yo why tf thre is a vertical scroll in the drawer man come on

--- src/components/route/route-history-sheet.tsx
+++ src/components/route/route-history-sheet.tsx
@@ -194,7 +194,9 @@ export function RouteHistorySheet(props: RouteHistorySheetProps) {
                         </Button>
                     </div>
                 </DrawerHeader>
-                <RouteHistoryContent />
+                <div className="flex-1 min-h-0">
+                  <RouteHistoryContent />
+                </div>
             </DrawerContent>
       </Drawer>
     )
